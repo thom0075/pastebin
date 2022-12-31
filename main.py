f6945d5b39734765872b431cd187017b185d718b
thoma <thomascellitti@aol.com>
2022-12-31 22:14:35 +0100

    Started writing TODOs

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 import requests
+
+#create basic UI
+#(1) the program must fetch user information
+#(2) dedicated window to paste content
+#(3) dedicated window to fetch content
+#the program must store api keys (encrypted)
+#and the user keys. It must also store fetched data in secure
+#files
+#(4) send the fetched data via e-mail
+
+api_dev_key = str()
